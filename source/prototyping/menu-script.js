897b1e277ca68e751a1dde5c09d2f3472e557998
josh-tan-20-09-13 <joshua555op@gmail.com>
2023-05-16 13:09:26 -0700

    Added more starter code for project
    The landing and menu pages now have starter code which have the
    basic underlying functionality as specified in the github issues.

---
+++ source/prototyping/menu-script.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll('button');
+
+/* Iterate through the buttons and set each one accordingly */
+for (let i = 0; i < categories.length; i++) {
+  categories[i].addEventListener('click', (event) => {
+    /* TODO: Set data in local storage based on clicked item */
+    window.location.href = 'card-prototype.html';
+  });
+}
