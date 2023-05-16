7bc94809ec39f684be6143f7123f8483cec95874
josh-tan-20-09-13 <joshua555op@gmail.com>
2023-05-16 12:22:04 -0700

    Added techdemo starter code to prototyping directory
    The prototyping directory serves as a folder for "starter code" which will
    not be used for the final production code but exists as a test and wireframe
    for the final code. Do not directly use the code without modification.

---
+++ source/prototyping/card-script.js
@@ -0,0 +1,85 @@
+/*
+This is a early test version for the tarot card software to
+verify that the back-end exists and is functional for the
+cse 110 class at UCSD.
+Authored by Joshua Tan on 05/12/2023
+*/
+
+/* Get objects */
+const setCardBtn = document.getElementById('setCount');
+const cardCount = document.getElementById('cardCount');
+const predictBtn = document.getElementById('getTarot');
+const onlyOneChk = document.getElementById('onlyOne');
+
+/* set number of cards based on input */
+setCardBtn.addEventListener("click", (event) => {
+  const cardWrapper= document.getElementById('cardWrapper');
+  if (cardCount.value == "")
+    cardCount.value = 6;
+  cardContent = "";
+
+  /* Generate and set contents */
+  for (let i = 0; i < cardCount.value; i++)
+    cardContent += `<tarot-card id=\"card-${i}\">I am a Card!</tarot-card>`;
+  cardWrapper.innerHTML = cardContent;
+});
+
+/* Get selected cards */
+predictBtn.addEventListener("click", (event) => {
+  /* Yes, this is dumb but we can customize responses this way */
+  const numbers = ["1","2","3","4","5","6","7","8","9","10"];
+
+  const predictOut = document.getElementById('output');
+  const selected = document.querySelectorAll('[pick=""]');
+  
+  if (selected.length !== 0) {
+    /* Generate output */
+    res = Math.floor(Math.random() * cardCount.value);
+    outputContent = "";
+    for (let i = 0; i < selected.length; i++)
+      outputContent += `${selected[i].id} `
+    predictOut.innerHTML = `<p>You selected the following: ${outputContent}<br>Here is a random number: ${numbers[res]}</p>`
+  } else
+    predictOut.innerHTML = `<p>You did not select anything stupid!<p>`
+    /* Always insult the user when they make a mistake */
+});
+
+/* Custom element here */
+class card extends HTMLElement {
+  /* Check if pick attribute exists */
+  get pick() {
+    return this.hasAttribute("pick");
+  }
+
+  /* assign pick and change color */
+  set pick(val) {
+    if (val) {
+      /* Deselect other cards */
+      if (onlyOneChk.checked) {
+        const cards = document.querySelectorAll("tarot-card");
+        for (let i = 0; i < cards.length; i++)
+          cards[i].pick = false;
+      }
+
+      /* Select this card */
+      this.setAttribute("pick", "");
+      this.style.backgroundColor = "#00ff00"
+    } else {
+      /* Deselect this card */
+      this.removeAttribute("pick", "");
+      this.style.backgroundColor = "#ff0000"
+    }
+  }
+
+  constructor() {
+    super();
+   
+    /* Add listener for each card */ 
+    this.addEventListener("click", (event) => {
+      this.pick = !this.pick;
+    });
+  }
+}
+
+/* Register element */
+customElements.define("tarot-card", card);
