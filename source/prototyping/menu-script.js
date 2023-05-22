afa3837a7a99f8d6e05d2c5094033f4e695b0573
bimai25 <bimai@ucsd.edu>
2023-05-21 17:15:35 -0700

    updated files to pass validators

--- source/prototyping/menu-script.js
+++ source/prototyping/menu-script.js
@@ -1,13 +1,13 @@
 /**
- * Array containing references to all the category buttons on menu-prototype.html 
- * @type {[HTMLCollectionOf<Element>]}
+ * Array containing references to all the category buttons on menu-prototype.html
+ * @type {HTMLCollectionOf<Element>}
  */
 const categories = document.getElementsByClassName('category');
 
 /**
  * Iterate through the categories and set onClick listeners to
  * each on to update localStorage with category chosen and
- * navigates user to card-prototype.html 
+ * navigates user to card-prototype.html
  */
 for (let i = 0; i < categories.length; i++) {
   categories[i].addEventListener('click', (event) => {
@@ -17,34 +17,33 @@ for (let i = 0; i < categories.length; i++) {
   });
 }
 
-
 /**
- * A reference to back button HTMlElement on menu-prototype.html 
- * @type {[HTMLElement | null]}
+ * A reference to back button HTMlElement on menu-prototype.html
+ * @type {HTMLElement | null}
  */
 const backButton = document.getElementById('back');
 
 /**
  * Adds an onClick listner to the reference to the back
- * button html element on menu-prototype.html so that when 
+ * button html element on menu-prototype.html so that when
  * clicked the user gets navigated to the Landing Page (landing-prototype.html)
  */
 backButton.addEventListener('click', () => {
   window.location.href = 'landing-prototype.html';
-})
+});
 
 
 /**
- * A reference to Saved Readings button HTMlElement on menu-prototype.html 
+ * A reference to Saved Readings button HTMlElement on menu-prototype.html
  * @type {[HTMLElement | null]}
  */
 const savedReadingsButton = document.getElementById('savedReadings');
 
 /**
  * Adds an onClick listner to the reference to the Saved Readings
- * button html element on menu-prototype.html so that when 
+ * button html element on menu-prototype.html so that when
  * clicked the user gets navigated to the Saved Readings Page (saved-readings-prototype.html)
  */
 savedReadingsButton.addEventListener('click', () => {
   window.location.href = 'saved-readings-prototype.html';
-})
+});
