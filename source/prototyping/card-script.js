dcd0e4fc41ed21c39108e2bda3d96b4345ea4d51
OutisNoMore <OutisNoMore@protonmail.com>
2023-05-23 21:57:25 -0700

    update to work with current version of main

--- source/prototyping/card-script.js
+++ source/prototyping/card-script.js
@@ -1,62 +1,175 @@
-/*
-This is a early test version for the tarot card software to
-verify that the back-end exists and is functional for the
-cse 110 class at UCSD.
-Authored by Joshua Tan on 05/12/2023
-*/
+/**
+ * A reference to the number of cards the user wants to select
+ * @type {int}
+ */
+let selectCount;
 
-/* Get objects */
-const setCardBtn = document.getElementById('setCount');
-const cardCount = document.getElementById('cardCount');
-const predictBtn = document.getElementById('getTarot');
-const selectCount = document.getElementById('selectCount');
-const newCategory = document.getElementById('newCategory');
+/**
+ * Set the number of cards to appear to be 6
+ * @type {int}
+ */
+let cardCount = 6;
+
+/**
+ * A reference to a button to get the tarot card predictions
+ * @type {HTMLElement | null}
+ */
+const predictButton = document.getElementById('getTarot');
+
+/**
+ * Array containing the id strings of all selected cards
+ * @type {string[]}
+ */
+let selectBuffer = [];
+
+/**
+ * A reference to back button HTMlElement on card-prototype.html
+ * @type {HTMLElement | null}
+ */
+const returnToMenuButton = document.getElementById('returnMenu');
+
+/**
+ * A reference to all card images 
+ * @type {HTMLCollection img | null}
+ */
 const tarotCards = document.getElementsByClassName('card');
 
-newCategory.addEventListener("click", (event) => {
-  window.location.href='menu-prototype.html';
-});
 
-// Array of selected cards, based on card number, from 0 to 5 for 6 cards
-let selectBuf = [];
+window.addEventListener('load', init);
 
-for (let i = 0; i < tarotCards.length; i++) {
-  tarotCards[i].addEventListener("click", (event) => {
-    const index = selectBuf.indexOf(i);
-    if (index == -1) {
-      /* Select this card */
-      selectBuf.push(i);
-      tarotCards[i].style.boxShadow = "0 0 10px 5px #ff0000";
+/**
+ * Function containing all intial setup functions for generating cards 
+ * and event listeners for the buttons on the page
+ */
+function init() {
+  for (let i = 0; i < tarotCards.length; i++) {
+    tarotCards[i].addEventListener("click", function () { chooseCard(i); });
+  }
 
-      /* Deselect other cards if buffer is full */
-      if (selectBuf.length > selectCount.value) {
-        /* remove selected cards from the selected buffer */
-        tarotCards[selectBuf[0]].style.boxShadow = null;
-        selectBuf.shift();
-      }
+  /* Register tarot-card custom element */
+  //customElements.define("tarot-card", card);
 
-    } else {
-      /* Deselect this card */
-      tarotCards[i].style.boxShadow = null;
+  /* Get category from local storage */
+  let category = JSON.parse(localStorage.getItem("category"));
 
-      /* Remove from selectBuf when deselecting a card */
-      selectBuf.splice(index, 1);
-    }
-  });
+  /* Set selectCount value from category */
+  switch (category) {
+    case "education":
+      selectCount = 3;
+      break;
+    case "love":
+    case "life":
+    default:
+      selectCount = 1;
+      break;
+  }
+
+  /* Generate visual images of cards */
+  //generateCards();
+
+  /* Add event listener for predicting fortune button */
+  predictButton.addEventListener("click", generatePrediction);
+
+  /* Add event listener for return to menu button to go back to menu page */
+  returnToMenuButton.addEventListener("click", returnToMenu);
 }
 
-/* Get selected cards */
-predictBtn.addEventListener("click", (event) => {
+function returnToMenu() {
+  window.location.href = "menu-prototype.html";
+}
+
+/**
+ * A function used for an event listener in order to generate the prediction
+ * when the user has selected their cards
+ */
+function generatePrediction() {
   /* 
-  TODO: Yes, this is dumb but we can customize responses this way 
-  Output will be selected based on the category and its specifications
-  */
+   * TODO: Write out the actual predictions and maybe make the prediction 
+   * algorithm smarter instead of PRNG 
+   */
+  
+  /**
+   * Array containing the strings of the responses from the tarot cards
+   * @type {string[]}
+   */
+  const numbers = ["1","2","3","4","5","6","7","8","9","10"];
+
+  /**
+   * A reference to the output area for the result of the reading
+   * @type {HTMLElement | null}
+   */
+  const predictOut = document.getElementById('output');
+
+  /* Verify items are selected */
+  if (selectBuffer && selectBuffer.length === selectCount) {
+    /* Get a random fortune */
+    res = Math.floor(Math.random() * cardCount);
+
+    /**
+     * String used for storing the output of the prediction
+     * @type {string}
+     */
+    let outputContent = "";
+
+    /* Add selected cards to output */
+    for (let i = 0; i < selectBuffer.length; i++)
+      outputContent += `${tarotCards[selectBuffer[i]].id} `;
+
+    /* Give the user a prediction */
+    predictOut.innerHTML = `<p>You selected the following: ${outputContent}<br>Here is a random number: ${numbers[res % selectBuffer.length]}</p>`;
+  } else {
+    /* Display a message that the user selected nothing */
+    predictOut.innerHTML = `<p>You did not select anything stupid!<p>`;
+  }
+}
+
+function chooseCard (i) {
+  const index = selectBuffer.indexOf(i);
+  if (index == -1) {
+    selectBuffer.push(i);
+    tarotCards[i].style.boxShadow = "0 0 10px 5px #ff0000";
+
+    if (selectBuffer.length > selectCount) {
+      tarotCards[selectBuffer[0]].style.boxShadow = null;
+      selectBuffer.shift();
+    }
+
+  } else {
+    tarotCards[i].style.boxShadow = null;
+
+    selectBuffer.splice(index, 1);
+  }
+}
+
+
+/*
+// Array of selected cards, based on card number, from 0 to 5 for 6 cards
+let selectBuffer = [];
+
+function chooseCard () {
+  const index = selectBuf.indexOf(i);
+  if (index == -1) {
+    selectBuf.push(i);
+    tarotCards[i].style.boxShadow = "0 0 10px 5px #ff0000";
+
+    if (selectBuf.length > selectCount.value) {
+      tarotCards[selectBuf[0]].style.boxShadow = null;
+      selectBuf.shift();
+    }
+
+  } else {
+    tarotCards[i].style.boxShadow = null;
+
+    selectBuf.splice(index, 1);
+  }
+}
+
+predictBtn.addEventListener("click", (event) => {
   const numbers = ["1","2","3","4","5","6","7","8","9","10"];
 
   const predictOut = document.getElementById('output');
   
   if (selectCount.value !== 0 && selectBuf.length == selectCount.value) {
-    /* Generate output */
     res = Math.floor(Math.random() * 6);
     outputContent = "";
     for (let i = 0; i < selectBuf.length; i++)
@@ -64,8 +177,8 @@ predictBtn.addEventListener("click", (event) => {
     predictOut.innerHTML = `<p>You selected the following: ${outputContent}<br>Here is a random number: ${numbers[res]}</p>`
   } else
     predictOut.innerHTML = `<p>You did not select anything stupid!<p>`
-    /* Always insult the user when they make a mistake */
 });
+*/
 
 /*
 // set number of cards based on input
--- source/prototyping/menu-script.js
+++ source/prototyping/menu-script.js
@@ -1,24 +1,91 @@
+/**
+ * @file Contains the event listeners for buttons on menu-prototype.html 
+ * @author Michi Wada
+ * @author Helen Lin
+ */
+
+/**
+ * A reference to the div containing all the wood-panels on menu-prototype.html
+ * @type {HTMLCollection<Element>}
+ */
 const categories = document.getElementById('categories');
+
+/**
+ * Array of wood-panels to create 
+ * @type {HTMLCollection<String>}
+ */
 const titles = [
+  "School",
   "Love",
-  "Future",
-  "Career",
-  "School"
+  "Life"
 ];
 
-window.addEventListener('DOMContentLoaded', createPanels);
+/**
+ * A reference to back button HTMlElement on menu-prototype.html
+ * @type {HTMLElement | null}
+ */
+const backButton = document.getElementById('back');
 
+/**
+ * A reference to Saved Readings button HTMlElement on menu-prototype.html
+ * @type {HTMLElement | null}
+ */
+const savedReadingsButton = document.getElementById('savedReadings');
+
+/**
+ * A function used for an event listener which is responsible for performing the 
+ * necessary actions when a card is clicked on by the user such as saving card 
+ * category type and redirecting them
+ */
+function setCardLink(i) {
+  /* Set data in local storage based on clicked category */
+  localStorage.setItem('category', JSON.stringify(titles[i]));
+  window.location.href = 'card-prototype.html';
+}
+
+/**
+ * A function used for an event listener which is responsible for performing the
+ * necessary actions when the landing page is clicked on by the user
+ */
+function setLandingLink() {
+  window.location.href = 'landing-prototype.html';
+}
+
+/**
+ * A function used for an event listener which is responsible for performing the
+ * necessary actions when the saved readings page is clicked
+ */
+function setSavedReadingsLink() {
+  window.location.href = 'saved-readings-prototype.html';
+}
+
+/**
+ * A function used for an event listener which is responsible for creating the
+ * wood panels with each category when the page is loaded
+ */
 function createPanels() {
   for (let i = 0; i < titles.length; i++) {
     let newCategory = document.createElement("wood-panel");
     newCategory.data = titles[i];
-    newCategory.addEventListener('click', (event) => {
-    /* TODO: Set data in local storage based on clicked item */
-      window.location.href = 'card-prototype.html';
-    });
-    newCategory.addEventListener('hover', (event) => {
-
+    newCategory.addEventListener('click', function () { 
+      setCardLink(i);
     });
     categories.appendChild(newCategory);
   }
 }
+
+window.addEventListener('DOMContentLoaded', createPanels);
+
+/*
+ * Adds an onClick listner to the reference to the back button html element on 
+ * menu-prototype.html so that when clicked the user gets navigated to the 
+ * Landing Page (landing-prototype.html)
+ */
+backButton.addEventListener('click', setLandingLink);
+
+/*
+ * Adds an onClick listner to the reference to the Saved Readings button html 
+ * element on menu-prototype.html so that when clicked the user gets navigated 
+ * to the Saved Readings Page (saved-readings-prototype.html)
+ */
+savedReadingsButton.addEventListener('click', setSavedReadingsLink);
