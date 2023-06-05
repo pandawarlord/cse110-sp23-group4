68e1843f87e523e5407576f1b9bbac6e0a9dd873
samuelau824 <seau@ucsd.edu>
2023-06-05 14:56:55 -0700

    add semicolons for js-linter test

--- source/__tests__/menu-page-test.js
+++ source/__tests__/menu-page-test.js
@@ -7,12 +7,12 @@ describe('Basic user flow for Menu Page', () => {
 
     beforeAll(async () => {
         //Note this is a personal Live Server link. So, it may not work in general. 
-        console.log("Menu page tests...")
+        console.log("Menu page tests...");
         await page.goto('http://127.0.0.1:8000/source/prototyping/menu-prototype.html');
     });
 
     test("Check if back button takes you back to landing page on click", async () => {
-        console.log("Checking if back button takes you back to landing page on click...")
+        console.log("Checking if back button takes you back to landing page on click...");
         const button = await page.$('#back'); 
         //console.log(await(await button.getProperty('innerText')).jsonValue())
         await button.click(); 
@@ -28,7 +28,7 @@ describe('Basic user flow for Menu Page', () => {
     });
 
     test("Check if saved readings button takes you to the saved readings page on click", async () => {
-        console.log("Checking if saved readings button takes you to the saved readings page on click...")
+        console.log("Checking if saved readings button takes you to the saved readings page on click...");
 
         const button = await page.$('#savedReadings'); 
         await button.click(); 
@@ -44,7 +44,7 @@ describe('Basic user flow for Menu Page', () => {
     });
 
     test("Check if all the category buttons take you to the card reading page on click", async () => {
-        console.log("Checking if all category buttons take you to the card reading page on click...")
+        console.log("Checking if all category buttons take you to the card reading page on click...");
 
         const buttons = await page.$('categoryButton').toArray(); 
 
