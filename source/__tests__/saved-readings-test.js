8a029382f690fb91bf6f5bcb021b0feb34c9dc70
abijitj <abijit.jay@gmail.com>
2023-06-05 15:09:44 -0700

    Fix site link

--- source/__tests__/saved-readings-test.js
+++ source/__tests__/saved-readings-test.js
@@ -6,7 +6,7 @@
 describe('Basic user flow for Saved Readings Page', () => {
     beforeAll(async () => {
         //Note this is a personal Live Server Link. So, it will not work in general.
-        await page.goto('http://127.0.0.1:5500/source/prototyping/saved-readings-prototype.html');
+        await page.goto('http://127.0.0.1:8000/source/prototyping/saved-readings-prototype.html');
     });
     test("Check if back button takes you back to the menu page on click", async() => {
         console.log("Checking if back button takes you back to the menu page on click.");
@@ -20,7 +20,7 @@ describe('Basic user flow for Saved Readings Page', () => {
         const page2Title = await page.title();
         console.log(page2Title);
 
-        expect(page2URL).toBe('http://127.0.0.1:5500/source/prototyping/menu-prototype.html');
+        expect(page2URL).toBe('http://127.0.0.1:8000/source/prototyping/menu-prototype.html');
         expect(page2Title).toBe('This is the menu page prototype');
     });
 });
