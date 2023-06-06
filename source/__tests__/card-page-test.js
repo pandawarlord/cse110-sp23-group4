53428c057cce87bf43928207cf2f737d0ce6ee2a
josh-tan-20-09-13 <joshua555op@gmail.com>
2023-06-05 18:39:27 -0700

    Added a selection test

--- source/__tests__/card-page-test.js
+++ source/__tests__/card-page-test.js
@@ -1,10 +1,10 @@
 describe('Basic user flow for Landing Page', () => {
     beforeAll(async () => {
-        //Note this is a personal Live Server link. So, it will not work in general. 
-        await page.goto('http://127.0.0.1:8000/source/prototyping/card-prototype.html');
+      await page.goto('http://127.0.0.1:8000/source/prototyping/card-prototype.html');
     });
 
     test("Verify user cannot predict without cards", async () => {
+      // Click without selecting cards
       await page.click('#getTarot');
       const outputText = await page.$eval('#output', (text) => {
         return text.innerHTML;
@@ -12,5 +12,21 @@ describe('Basic user flow for Landing Page', () => {
       expect(outputText).toBe('<p>You did not select enough cards!</p><p></p>');
     });
 
-    
+    test("Verify user cannot predict a second time", async() => {
+      // Select a Card
+      await page.click('#card1');
+
+      await page.click('#getTarot');
+      const outputText = await page.$eval('#output', (text) => {
+        return text.innerHTML;
+      });
+
+      for (let i = 0; i < 10; i++) {
+        await page.click('#getTarot');
+        const newText = await page.$eval('#output', (text) => {
+          return text.innerHTML;
+        });
+        expect(outputText).toBe(newText);
+      }
+    });
 });
